62aa3c167db84bba9004cdb5359e60dc7c297bcf
David Aguina <aguinadavid@gmail.com>
2022-05-30 11:19:31 -0300

    resolucao_ex_24_estrutura_de_decisao

--- secao_02_estrutura_de_decisao/ex_24_operacao.py
+++ secao_02_estrutura_de_decisao/ex_24_operacao.py
@@ -31,3 +31,38 @@ Mostre o restultado com duas casas decimais
 
 def fazer_operacao_e_classificar(n_1: float, n_2: float, operacao: str):
     """Escreva aqui em baixo a sua solução"""
+    if operacao == '+':
+      resultado = n_1 + n_2 
+    elif operacao == '-':
+      resultado = n_1 - n_2
+    elif operacao == '*':
+      resultado = n_1 * n_2
+    elif operacao == '/':
+      resultado = n_1 / n_2
+    if resultado > 0:
+      sinal = 'positivo'
+    elif resultado < 0:
+      sinal = 'negativo'
+    else:
+      sinal = 'neutro'
+    resultado_str = str(round(resultado, 2)).split(".")
+    if len(resultado_str) == 1:
+      decimal_ou_inteiro = 'inteiro'
+    else:
+      if round(int(resultado_str[1])) == 0:
+        decimal_ou_inteiro = 'inteiro'
+      else:
+        decimal_ou_inteiro = 'decimal'
+    resto_por_2 = resultado % 2
+    if resto_por_2 == 0:
+      paridade = 'par'
+    else:
+      paridade = 'impar'
+    print(f"Resultado: {resultado:.2f}")
+    if decimal_ou_inteiro == 'inteiro':
+      print(f"Número é {paridade}, {sinal} e {decimal_ou_inteiro}.")
+    else:
+      print(f"Número é {sinal} e {decimal_ou_inteiro}.")
+
+
+
