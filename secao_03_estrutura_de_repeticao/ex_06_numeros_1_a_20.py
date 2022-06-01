eb13c13d45129643e5c2f257f196a7ebd63e427a
David Aguina <aguinadavid@gmail.com>
2022-06-01 01:01:36 -0300

    resolucao_ex_06_estrutura_de_decisao

--- secao_03_estrutura_de_repeticao/ex_06_numeros_1_a_20.py
+++ secao_03_estrutura_de_repeticao/ex_06_numeros_1_a_20.py
@@ -33,3 +33,14 @@ mostre os números um ao lado do outro.
 
 def escrever_numeros_de_1_a_20(formato:str) -> str:
     """Escreva aqui em baixo a sua solução"""
+    if formato == 'um_abaixo_do_outro':
+        for n in range(1, 21, 1):
+            print(n)
+    elif formato == 'um_ao_lado_do_outro':
+        resultado =''
+        for n in range(1, 20, 1):
+            print(n, end=' ')
+        else:
+            print(n+1)
+        
+
