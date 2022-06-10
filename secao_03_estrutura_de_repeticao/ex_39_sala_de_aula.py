06a673f3d375bfa43260d8a1ef669ff584ca7f10
David Aguina <aguinadavid@gmail.com>
2022-06-10 11:58:11 -0300

    resolucao_ex_39_estrutura_de_repeticao

--- secao_03_estrutura_de_repeticao/ex_39_sala_de_aula.py
+++ secao_03_estrutura_de_repeticao/ex_39_sala_de_aula.py
@@ -17,3 +17,17 @@ representando a sua altura em centímetros. Encontre o aluno mais alto e o mais 
 
 def calcular_aluno_mais_baixo_e_mais_alto(*alunos) -> str:
     """Escreva aqui em baixo a sua solução"""
+    lista_alunos = []
+    for nome, altura in alunos:
+        lista_alunos.append([nome,altura])
+    mais_alto = mais_baixo = int(lista_alunos[0][1])
+    nome_mais_alto = nome_mais_baixo = lista_alunos[0][0]
+    i = 0
+    for i in range(len(lista_alunos)):
+        if int(lista_alunos[i][1]) > mais_alto:
+            mais_alto = int(lista_alunos[i][1])
+            nome_mais_alto = lista_alunos[i][0]
+        if int(lista_alunos[i][1]) < mais_baixo:
+            mais_baixo = int(lista_alunos[i][1])
+            nome_mais_baixo = lista_alunos[i][0]
+    print(f"'O maior aluno é o {nome_mais_alto} com {mais_alto} cm. O menor aluno é o {nome_mais_baixo} com {mais_baixo} cm'")
